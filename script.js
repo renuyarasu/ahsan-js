873de446b9e53fb60e460dee402d07c24dc07320
Renuka Prasad <renu.yarasu@gmail.com>
2023-02-01 18:19:43 +0530

    const

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 // Beginners JavaScript Series 2021 (Muhammad Ahsan Ayaz) - https://www.youtube.com/playlist?list=PL2sQdFoGnLIjERHiLVzcheUODYLPMiUx3
 console.clear();
 
-let country = 'USA';
+const PI = 5.2323;
 
-let i = 0;
 function main() {
-    let i = 100;
-    let country = 'India';
-    console.log(i);
-    console.log(country);
+    const PI = 5.000;
 }
 main()
-console.log(i);
-console.log(country);
+console.log(PI);
