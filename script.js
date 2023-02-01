168423b2621b6c8656f732f3cbdd72342a68e70c
Renuka Prasad <renu.yarasu@gmail.com>
2023-02-01 18:14:39 +0530

    var

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // Beginners JavaScript Series 2021 (Muhammad Ahsan Ayaz) - https://www.youtube.com/playlist?list=PL2sQdFoGnLIjERHiLVzcheUODYLPMiUx3
 console.clear();
+
+var country = 'USA';
+
+function main() {
+    var country = 'India';
+    console.log(country);
+}
+main()
+console.log(country);
